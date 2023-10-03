2e69846d37b7f490afd00c8df1284149b67b0f8b
backdevhong <ghddls0704@kakao.com>
2023-10-04 02:45:45 +0900

    front

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useRef } from "react";
+import { connect } from "socket.io-client";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const addEle = useRef<HTMLDivElement>(null);
+
+  const webSocketURL = process.env.REACT_APP_CONNECT_URL as string;
+
+  const socket = connect(webSocketURL, {
+    path: "/socket.io",
+    transports: ["websocket"],
+  });
+
+  socket.on("data", (data) => {
+    const namespan = document.createElement("span");
+    namespan.classList.add("name");
+    const name = document.createTextNode(data.username);
+    namespan.appendChild(name);
+
+    const colonspan = document.createElement("span");
+    colonspan.classList.add("colon");
+    const colon = document.createTextNode(":");
+    colonspan.appendChild(colon);
+
+    const metaspan = document.createElement("span");
+    metaspan.classList.add("meta");
+    metaspan.append(namespan, colonspan);
+    const messagespan = document.createElement("span");
+
+    messagespan.classList.add("message");
+    const message = document.createTextNode(data.content);
+    messagespan.appendChild(message);
+
+    const container = document.createElement("div");
+    container.classList.add("container");
+    container.append(metaspan, messagespan);
+
+    addEle.current?.appendChild(container);
+  });
+
+  return <div id="log" ref={addEle}></div>;
 }
 
 export default App;
